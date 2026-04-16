b3a1905544b0e2bd7e0c55eed3d593ae5a85d958
lizzyman04 <arlindoabdullm@gmail.com>
2026-04-16 19:59:48 +0200

    chore: include services barrel file

---
+++ lib/services/index.ts
@@ -0,0 +1,7 @@
+export * from './classes';
+export * from './scores';
+export * from './sessions';
+export * from './students';
+export * from './subjects';
+export * from './teachers';
+export * from './client';
